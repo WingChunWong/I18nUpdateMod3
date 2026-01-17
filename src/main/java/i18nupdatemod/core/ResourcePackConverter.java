eafdb5266b15af29df171ce2a73a9f20a060b8d8
WingChunWong <huangrongjun2200@outlook.com>
2026-01-17 12:37:27 +0800

    仅在 `pack format` > `64 `时,引入 `min_format` 和 `max_format`

--- src/main/java/i18nupdatemod/core/ResourcePackConverter.java
+++ src/main/java/i18nupdatemod/core/ResourcePackConverter.java
@@ -77,8 +77,13 @@ public class ResourcePackConverter {
     private byte[] convertPackMeta(InputStream is, int packFormat, String description) {
         PackMeta meta = GSON.fromJson(new InputStreamReader(is, StandardCharsets.UTF_8), PackMeta.class);
         meta.pack.pack_format = packFormat;
-        meta.pack.min_format = packFormat;
-        meta.pack.max_format = packFormat;
+        if (packFormat > 64) {
+            meta.pack.min_format = packFormat;
+            meta.pack.max_format = packFormat;
+        } else {
+            meta.pack.min_format = null;
+            meta.pack.max_format = null;
+        }
         meta.pack.description = description;
         return GSON.toJson(meta).getBytes(StandardCharsets.UTF_8);
     }
@@ -88,8 +93,8 @@ public class ResourcePackConverter {
 
         private static class Pack {
             int pack_format;
-            int min_format;
-            int max_format;
+            Integer min_format;
+            Integer max_format;
             String description;
         }
     }
